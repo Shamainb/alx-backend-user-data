8edf1255ad82fe0e335c80c1135c2d5252199872
Shamainb <shamainbhengesa60@gmail.com>
2024-05-30 19:45:19 +0000

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Arguments:
+    fields: a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character
+    is separating all fields in the log line (message)
+    """
+    pattern = f'({separator})({"|".join(fields)})(?={separator}|$)'
+    return re.sub(pattern, redaction, message)
