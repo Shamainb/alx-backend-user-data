3e01cc7544de747827f3c7e8b36a396a7dc3df9a
Shamainb <shamainbhengesa60@gmail.com>
2024-05-30 20:40:50 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        def filter_datum(datum):
+            return self.REDACTION if datum in self.fields else datum
 
 
 def filter_datum(fields, redaction, message, separator):
